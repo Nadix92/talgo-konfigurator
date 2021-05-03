2928caffc1e06fb1462f16efd5a62327a508c607
Nadix92 <49558241+Nadix92@users.noreply.github.com>
2021-05-03 12:58:49 +0200

    changes to model and toggles

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const _container = document.getElementById('sdv-container');
+// const ticket =
+//   '26dc1972fa619ed5b5d9cc918a4bf2e5ddd323757d9cd4da623cc4fef6633311b1bf1bf3f2d69c2925c614bc6727b36329933e55254449d2bd07652909161d00822f888f82383f3b293325b990822c69f2eacdff6c5ad8f2f46ea911cd95d61bb3865a950f425e109bb50e9795677a22b4ca10e362d8-18541cd39125a5caf165af96da6d6f40';
+// https://app.shapediver.com/m/talgoe-v1-18
+
 const ticket =
-  '9ceb4ab676e5aa1767cabffa734e04210c9342d392d448e867a58e203ce93c2e64884da3e8dd64a785f6212e4b971472103e265698d8945298c6d6155d6c91ea5904f2cc7b6a12d9ef618584235b117b39b13ed809296597f8de1f9894879282b9006247f7442a322722ae3958df241a3d19da32d65a-cf09a421d217510a300a8ca73c578b0f';
-// https://app.shapediver.com/m/talgoe-v1-4
+  'e8520a550bc7683c06b52617d892c76fc65a5abc7c8d0f4ff83a82c51581cded87de40568ccac93e51a396f0a8d1171c2de914c10c3f436787d0ad4eb927ae6cfd0b0ce69c31920380816ff2844feddd6397ae4026397b9a633381d27287404e9d34dfb8ae96000c3cae7fdb9108f39ff189a47681fb-355b54a23ebcad7f5c20ece057cf266d';
+// https://app.shapediver.com/m/talgoe-v1-17
 
 const _viewerSettings = {
   container: _container,
@@ -22,15 +26,20 @@ let viewerInit = false;
 const applyBtn = document.getElementById('apply-btn');
 const pName = document.getElementById('name');
 const pVal = document.getElementById('value');
-const viewName = document.getElementById('view-name');
-const formContainer = document.getElementById('view-container');
 
 const min = document.getElementById('min');
 const max = document.getElementById('max');
 
-const visMål = document.getElementById('vis-mål');
+const download3dModel = document.getElementById('download-3d-model');
+const downloadPDF = document.getElementById('download-pdf');
+const spinner = document.getElementById('spinner');
+
 const visTegning = document.getElementById('vis-tegning');
-const downloadBtn = document.getElementById('download-btn');
+
+// const viewName = document.getElementById('view-name');
+// const formContainer = document.getElementById('view-container');
+// const visMål = document.getElementById('vis-mål');
+// const downloadBtn = document.getElementById('download-btn');
 
 api.scene.addEventListener(api.scene.EVENTTYPE.VISIBILITY_ON, function () {
   if (!viewerInit) {
@@ -49,15 +58,23 @@ api.scene.addEventListener(api.scene.EVENTTYPE.VISIBILITY_ON, function () {
     console.log('Export Data');
     console.log(resExport.data);
 
+    // api.parameters.updateAsync({
+    //   name: 'Vis 2D tegning',
+    //   value: true,
+    // });
+
     //////////////////////////////////////
     // Update Parameter
     const updateParameters = async (name, value) => {
       try {
-        // Temp solution to hide all "tegning" elements when updateing any paramters for better performance
-        // Ca 2 sec computeing uten tegning
-        // ca 5 sec med tegning
+        // Toggle off tegning before update any parameter
+        // togglePDF(false);
         visTegning.checked = false;
         showTegning();
+        // await api.parameters.updateAsync({
+        //   name: 'Vis 2D tegning',
+        //   value: false,
+        // });
 
         const response = await api.parameters.updateAsync({
           name,
@@ -80,7 +97,7 @@ api.scene.addEventListener(api.scene.EVENTTYPE.VISIBILITY_ON, function () {
 
     const getMinMaxValue = () => {
       //  get the the param that matches name of param input name/value
-      const filtered = parameters.data.find((element) => element.name === pName.value);
+      const filtered = parameters.data.find(element => element.name === pName.value);
       min.innerText = `${filtered.name} min value: ${filtered.min}`;
       max.innerText = `${filtered.name} max value: ${filtered.max}`;
 
@@ -97,54 +114,50 @@ api.scene.addEventListener(api.scene.EVENTTYPE.VISIBILITY_ON, function () {
 
     pName.addEventListener('change', getMinMaxValue);
 
-    // Drop down view
-    viewName.addEventListener('change', async () => {
-      try {
-        await api.parameters.updateAsync({
-          name: 'View',
-          value: viewName.value,
-        });
-      } catch (err) {
-        console.log(err);
-      }
-    });
-
     applyBtn.addEventListener('click', () => {
       updateParameters(pName.value, pVal.value);
     });
 
-    visMål.addEventListener('change', async () => {
-      try {
-        await api.parameters.updateAsync({
-          name: 'Vis mål',
-          value: visMål.checked,
+    const directExport = name => {
+      spinner.removeAttribute('hidden');
+      api.exports
+        .requestAsync({ name })
+        .catch(function (err) {
+          return Promise.reject(err);
+        })
+        .then(function (response) {
+          spinner.setAttribute('hidden', '');
+          if (response.data.msg) alert(response.data.msg);
+
+          console.log(response.data);
+
+          if (response.data.content.length !== 0) {
+            console.log('File format: ', response.data.content[0].format);
+            console.log('File size: ', response.data.content[0].size);
+            console.log('Download link: ', response.data.content[0].href);
+
+            window.location.href = response.data.content[0].href;
+          }
         });
+    };
+
+    downloadPDF.addEventListener('click', async () => {
+      try {
+        visTegning.checked = true;
+        showTegning();
+        // togglePDF(true);
+        directExport('DownloadPDF');
       } catch (err) {
         console.log(err);
       }
     });
 
+    download3dModel.addEventListener('click', directExport.bind(null, 'Download3dModel'));
+    // downloadPDF.addEventListener('click', directExport.bind(null, 'DownloadPDF'));
+
     // Toggle tegning
     const showTegning = async () => {
       try {
-        if (visTegning.checked) {
-          downloadBtn.classList.remove('hidden');
-          formContainer.classList.remove('hidden');
-
-          // Set Drop down to the defualt value
-          const filtered = parameters.data.find((element) => element.name === 'View');
-          viewName.value = filtered.defval;
-
-          // View Dropdown
-          await api.parameters.updateAsync({
-            name: 'View',
-            value: filtered.defval,
-          });
-        } else {
-          downloadBtn.classList.add('hidden');
-          formContainer.classList.add('hidden');
-        }
-
         // View toggle (true or false)
         await api.parameters.updateAsync({
           name: 'Vis 2D tegning',
@@ -154,26 +167,93 @@ api.scene.addEventListener(api.scene.EVENTTYPE.VISIBILITY_ON, function () {
         console.log(err);
       }
     };
-
     visTegning.addEventListener('change', showTegning);
 
-    downloadBtn.addEventListener('click', () => {
-      api.exports
-        .requestAsync({ name: 'Download2d' })
-        .catch(function (err) {
-          return Promise.reject(err);
-        })
-        .then(function (response) {
-          alert(response.data.msg);
-          console.log(response.data);
+    // value will be ture or false
+    // const togglePDF = async value => {
+    //   try {
+    //     await api.parameters.updateAsync({
+    //       name: 'FrontPDF',
+    //       value,
+    //     });
 
-          // console.log('File format: ', response.data.content[0].format);
-          // console.log('File size: ', response.data.content[0].size);
-          // console.log('Download link: ', response.data.content[0].href);
-        });
-    });
+    //     await api.parameters.updateAsync({
+    //       name: 'PerspectivePDF',
+    //       value,
+    //     });
+
+    //     await api.parameters.updateAsync({
+    //       name: 'TopPDF',
+    //       value,
+    //     });
+
+    //     await api.parameters.updateAsync({
+    //       name: 'RightPDF',
+    //       value,
+    //     });
+    //   } catch (err) {
+    //     console.log(err);
+    //   }
+    // };
 
     // This need to be Last on the code
     viewerInit = true;
   }
 });
+
+// ---------- OLD CODE FOR REFRENCE --------------------------
+
+// Drop down view
+// viewName.addEventListener('change', async () => {
+//   try {
+//     await api.parameters.updateAsync({
+//       name: 'View',
+//       value: viewName.value,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+// visMål.addEventListener('change', async () => {
+//   try {
+//     await api.parameters.updateAsync({
+//       name: 'Vis mål',
+//       value: visMål.checked,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+// Toggle tegning
+// const showTegning = async () => {
+//   try {
+//     if (visTegning.checked) {
+//       downloadBtn.classList.remove('hidden');
+//       formContainer.classList.remove('hidden');
+
+//       // Set Drop down to the defualt value
+//       const filtered = parameters.data.find(element => element.name === 'View');
+//       viewName.value = filtered.defval;
+
+//       // View Dropdown
+//       await api.parameters.updateAsync({
+//         name: 'View',
+//         value: filtered.defval,
+//       });
+//     } else {
+//       downloadBtn.classList.add('hidden');
+//       formContainer.classList.add('hidden');
+//     }
+
+//     // View toggle (true or false)
+//     await api.parameters.updateAsync({
+//       name: 'Vis 2D tegning',
+//       value: visTegning.checked,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+// visTegning.addEventListener('change', showTegning);
